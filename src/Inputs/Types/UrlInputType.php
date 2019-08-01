4963107c2b2700e381606d64d4b8783c3cf9e15f
Greg Gunner <greg@co-foundry.co.za>
2019-08-01 11:47:52 +0200

    Adding MinMax to the UrlInputType

--- src/Inputs/Types/UrlInputType.php
+++ src/Inputs/Types/UrlInputType.php
@@ -2,12 +2,16 @@
 
 namespace Foundry\Core\Inputs\Types;
 
+use Foundry\Core\Inputs\Types\Traits\HasMinMax;
+
 /**
  * Class UrlInputType
  *
  * @package Foundry\Requests\Types
  */
 class UrlInputType extends InputType {
+
+	use HasMinMax;
 
 	public function __construct( string $name, string $label = null, bool $required = true, $value = null, string $position = 'full', string $rules = null, string $id = null, string $placeholder = null, string $type = 'text' ) {
 		parent::__construct( $name, $label, $required, $value, $position, $rules, $id, $placeholder, $type );
