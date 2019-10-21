060c16b1159bf63ea35da637cab1b86d1187ba29
Greg Gunner <greg@co-foundry.co.za>
2019-10-21 12:07:51 +0200

    Adjusting the BrowseRequest trait to have a new method for getting browse related meta from the request

---
+++ src/Requests/Traits/BrowseableRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Foundry\Core\Requests\Traits;
+
+trait BrowseableRequest {
+
+    /**
+     * Extracts meta data from the request for use with the browse
+     *
+     * @param $page
+     * @param $limit
+     * @param $sortBy
+     * @param $sortDesc
+     * @return array
+     */
+    protected function getBrowseMeta($page, $limit, $sortBy, $sortDesc): array
+    {
+        $page = $this->input('page', $page);
+        $limit = $this->input('limit', $limit);
+        $sortBy = $this->input('sortBy', $sortBy);
+        $sortDesc = ($this->input('sortDesc', $sortDesc) === 'true') ? true : false;
+        return array($page, $limit, $sortBy, $sortDesc);
+    }
+
+}
