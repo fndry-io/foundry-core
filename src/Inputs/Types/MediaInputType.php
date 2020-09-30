e797a962d028d52dc66651a9480a5d858caa556b
Greg Gunner <greg@co-foundry.co.za>
2020-09-30 19:39:35 +0200

    Updating the MediaInputType to have the ability to set the allowed mimetypes on a media input

--- src/Inputs/Types/MediaInputType.php
+++ src/Inputs/Types/MediaInputType.php
@@ -5,6 +5,7 @@ namespace Foundry\Core\Inputs\Types;
 use Foundry\Core\Inputs\Types\Contracts\IsMultiple;
 use Foundry\Core\Inputs\Types\Traits\HasMinMax;
 use Foundry\Core\Inputs\Types\Traits\HasMultiple;
+use Symfony\Component\Mime\MimeTypes;
 
 /**
  * Class FileType
@@ -30,6 +31,49 @@ class MediaInputType extends InputType implements IsMultiple {
 	) {
 		$type = 'media';
 		parent::__construct( $name, $label, $required, $value, $position, $rules, $id, $placeholder, $type );
+        $this->setAttribute('mimeTypes', []);
 	}
+
+	public function setMimeTypes(array $types = [])
+    {
+        $this->setAttribute('mimeTypes', $types);
+        return $this;
+    }
+
+    public function addMimeTypes(array $types)
+    {
+        $this->setAttribute('mimeTypes', array_merge($this->getMimeTypes(), $types));
+        return $this;
+    }
+
+    public function getMimeTypes()
+    {
+        return $this->getAttribute('mimeTypes');
+    }
+
+    public function addImagesTypes()
+    {
+        $this->addMimeTypes([
+            'image/jpeg',
+            'image/png',
+            'image/gif',
+            'image/svg'
+        ]);
+        return $this;
+    }
+
+    public function getAllowedExtensions()
+    {
+        $extensions = [];
+        $mimeTypes = new MimeTypes();
+
+        foreach($this->getMimeTypes() as $type) {
+            $extensions = [
+                ...$extensions,
+                ...$mimeTypes->getExtensions($type)
+            ];
+        }
+        return $extensions;
+    }
 
 }
